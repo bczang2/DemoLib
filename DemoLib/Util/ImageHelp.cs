10826a0c2c8c42ea720a911363552b9debc55065
zangbc <zangbc@qq.com>
2017-09-22 14:09:09 +0800

    QRCode

---
+++ DemoLib/Util/ImageHelp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib.Util
+{
+    public class ImageHelp
+    {
+        /// <summary>
+        /// 将正方形图片切为圆形
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="rec"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Image CutEllipse(Image img, Rectangle rec, Size size)
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(size.Width, size.Height);
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    using (TextureBrush br = new TextureBrush(img, System.Drawing.Drawing2D.WrapMode.Clamp, rec))
+                    {
+                        br.ScaleTransform(bitmap.Width / (float)rec.Width, bitmap.Height / (float)rec.Height);
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                        g.FillEllipse(br, new Rectangle(Point.Empty, size));
+                    }
+                }
+                return bitmap;
+            }
+            catch (Exception ex){}
+
+            return null;
+        }
+    }
+}
---
+++ DemoLib/Util/QRCodeHelp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace DemoLib.Util
+{
+    public class QRCodeHelp
+    {
+        /// <summary>
+        /// 生成二维码
+        /// </summary>
+        /// <param name="qrString">二维码内容</param>
+        /// <param name="character">编码</param>
+        /// <param name="width">宽</param>
+        /// <param name="height">高</param>
+        /// <param name="margin">设置二维码的边距,单位不是固定像素</param>
+        /// <param name="level">容错级别</param>
+        /// <returns></returns>
+        public static Bitmap CreateQRCode(string qrString, string character, int width, int height, int margin = 0, string level = "H")
+        {
+            try
+            {
+                BarcodeWriter writer = new BarcodeWriter();
+                writer.Format = BarcodeFormat.QR_CODE;
+                //可选参数
+                QrCodeEncodingOptions options = new QrCodeEncodingOptions();
+                options.DisableECI = true;
+                options.CharacterSet = character;
+                options.Width = width;
+                options.Height = height;
+                options.Margin = margin;
+                options.ErrorCorrection = InitialErrorCorrection(level);
+                writer.Options = options;
+
+                Bitmap map = writer.Write(qrString);
+                return map;
+            }
+            catch (Exception ex) { }
+
+            return null;
+        }
+
+        private static ErrorCorrectionLevel InitialErrorCorrection(string level)
+        {
+            ErrorCorrectionLevel ret = ErrorCorrectionLevel.H;
+            if (!string.IsNullOrWhiteSpace(level))
+            {
+                switch (level)
+                {
+                    case "H":
+                        ret = ErrorCorrectionLevel.H;
+                        break;
+                    case "M":
+                        ret = ErrorCorrectionLevel.M;
+                        break;
+                    case "L":
+                        ret = ErrorCorrectionLevel.L;
+                        break;
+                    case "Q":
+                        ret = ErrorCorrectionLevel.Q;
+                        break;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
