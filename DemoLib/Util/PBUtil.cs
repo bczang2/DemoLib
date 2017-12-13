b556a185c794d9169b44b5c5186873127534c1a7
zangbc <zangbc@qq.com>
2017-12-13 14:39:16 +0800

    PB

---
+++ DemoLib/Util/PBUtil.cs
@@ -0,0 +1,51 @@
+﻿using DemoLib.PB;
+using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib.Util
+{
+    public class PBUtil<T> where T : IMessage<T>
+    {
+        /// <summary>
+        /// PB序列化
+        /// </summary>
+        /// <param name="fromObj"></param>
+        /// <returns></returns>
+        public static byte[] Serialize(T entity)
+        {
+            byte[] result = null;
+            if (entity != null)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    entity.WriteTo(ms);
+                    result = ms.ToArray();
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// PB反序列化
+        /// </summary>
+        /// <param name="jsonStr"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static T Deserialize(byte[] bytes, MessageParser<T> parser)
+        {
+            T result = default(T);
+            if (bytes != null && bytes.Length > 0)
+            {
+                result = parser.ParseFrom(bytes);
+            }
+
+            return result;
+        }
+    }
+}
